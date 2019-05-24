9f22ab1a11ddc73a4cd9d3b80791e4cf192d4f17
koppolusaikumar <50941625+koppolusaikumar@users.noreply.github.com>
2019-05-24 19:31:45 +0530

    Add files via upload

--- reducer.js
+++ reducer.js
@@ -1,30 +1,46 @@
 const intialState = {
-    cardNumber: -1,
-    cardArrayNames:[]
-  }
-   
-  const rootReducer = (state = intialState, action) => {
-      switch (action.type) {
-        case 'ADD_NEW_CARD':
-          return Object.assign({}, state, {
-            cardArrayNames: [
-              ...state.cardArrayNames,
-              {
-                name: action.name,
-                id:state.cardNumber+1
-              }
-            ],
-            cardNumber:state.cardNumber+1
-          });
-         case 'DELETE_CARD':
-         const filteredNames = state.cardArrayNames.filter((val) => val.id !== action.id)
-         console.log('delId',action.id);
-         console.log('del',filteredNames);
-         return { ...state,
+  cardNumber: 0,
+  innerStory:1,
+  cardArrayNames:[],
+  innerCard:[]
+}
+ 
+const rootReducer = (state = intialState, action) => {
+    switch (action.type) {
+      case 'ADD_NEW_CARD':
+        return Object.assign({}, state, {
+          cardArrayNames: [
+            ...state.cardArrayNames,
+            {
+              name: action.name,
+              id:state.cardNumber+1,
+            }
+          ],
+          cardNumber:state.cardNumber+1
+        });
+ 
+       case 'DELETE_CARD':
+        const filteredNames = state.cardArrayNames.filter((val) => val.id !== action.id)
+        return { ...state,
           cardArrayNames: filteredNames}
-        default:
-          return state
-      }
+ 
+      case 'ADD_INNER_CARD':
+      return Object.assign({}, state, {
+        innerCard: [
+          ...state.innerCard,
+          {
+       name:'User Story',
+            id:action.id,
+            story:state.innerStory
+          }
+        ],
+        innerStory:state.innerStory+1
+      });
+      default:
+        return state
     }
-    
-    export default rootReducer;
+  }
+  
+  export default rootReducer
+
+
